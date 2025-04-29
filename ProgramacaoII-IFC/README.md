a688be7902df2db8d550254ff31ddf34219d69e5
TimoteoRJanzen <timoteojanzen123@gmail.com>
2025-04-29 10:42:42 -0300

    Remove ProgramacaoII-IFC (refazer subtree sem squash)

--- ProgramacaoII-IFC/README.md
+++
@@ -1,1 +0,0 @@
-"# ProgramacaoII-IFC" 
--- ProgramacaoII-IFC/html/pessoa.py
+++
@@ -1,9 +0,0 @@
-class Pessoa():
-
-    def __init__(self,cpf,nome,endereco,telefone):
-        self.cpf = cpf
-        self.nome = nome
-        self.endereco = endereco
-        self.telefone = telefone
-
-lista = [Pessoa(1,"Zé","Rua aleatoria","90238"),Pessoa(2,"Alfred","Rua Baricentrica Central","5317-67473"),Pessoa(3,"EuExisto","Verdadeira.tx","57321G")]
